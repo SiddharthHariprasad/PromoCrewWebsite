5c7bc75cde84675f8aac355637e0c7ad6cec1021
SiddharthHariprasad <siddhuhariprasad@gmail.com>
2022-05-02 18:25:17 +0530

    added yesthra banner

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -13,43 +13,49 @@ function MainPage() {
                 </Carousel>
             </div>
             
-            {/* Parallax */}'
-            {/* <Parallax image={<img src={banner3} alt="banner3" className="responsive-img" />} options={{responsiveThreshold: 0}} /> */}
+            <div className="carousel-holder red" id="image-carousel">
+                {/* Photo Carousel */}
+                <Carousel carouselId="Carousel-1" options={{ fullWidth: true}}>
+                    {/* carousel div where the links and images */}
+                    <div className="black carousel-image-2">
+                        <a href="https://yesthra.com/" className="carousel-link" alt="go to branding">.</a>
+                    </div>
+                </Carousel>
+            </div>
+
             {/* Feedback */}
-            <section className="container section" id="feedback">
+            {/* <section className="container section mainPage" id="feedback"> */}
                 {/* new row inside the section for feedback title */}
-                <Row className="center-align">
+                {/* <Row className="center-align"> */}
                     {/* first column full size */}
-                    <Col s={12}>
-                        <h2>Testimonials</h2>
-                    </Col>
-                </Row>
+                    {/* <Col s={12}> */}
+                        {/* <h2>Testimonials</h2> */}
+                    {/* </Col> */}
+                {/* </Row> */}
                 {/* new row for feedbacks */}
-                <Row className="center-align">
+                {/* <Row className="center-align"> */}
                     {/* first column */}
-                    <Col s={12} l={6}>
+                    {/* <Col s={12} l={6}> */}
                         {/* card div to give it card like styling */}
-                        <CardPanel className="card-panel center-align">
+                        {/* <CardPanel className="card-panel center-align"> */}
                             {/* content of the card */}
-                            <h3>Lorem Ipsum</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est tempore reprehenderit aut esse iure laborum fugit quisquam sequi cumque aliquid. Provident unde explicabo eaque nihil atque earum id, minima quibusdam.
-                            </p>
-                        </CardPanel>
-                    </Col>
+                            {/* <h3>Lorem Ipsum</h3> */}
+                            {/* <p> */}
+                                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Est tempore reprehenderit aut esse iure laborum fugit quisquam sequi cumque aliquid. Provident unde explicabo eaque nihil atque earum id, minima quibusdam. */}
+                            {/* </p> */}
+                        {/* </CardPanel> */}
+                    {/* </Col> */}
                     {/* second column */}
-                    <Col s={12} l={6}>
-                        <CardPanel className="card-panel center-align">
-                            <h3>Lorem Ipsum</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem deserunt dignissimos dolores fuga. Quidem, id, temporibus esse quos officiis ipsam explicabo sit tempora labore sapiente provident sint quis iste est.
-                            </p>
-                        </CardPanel>
-                    </Col>
-                </Row>
-            </section>
-            {/* Parallax */}
-            {/* <Parallax image={<img src={banner4} alt="banner4" className="responsive-img" />} options={{responsiveThreshold: 0}} /> */}
+                    {/* <Col s={12} l={6}> */}
+                        {/* <CardPanel className="card-panel center-align"> */}
+                            {/* <h3>Lorem Ipsum</h3> */}
+                            {/* <p> */}
+                                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem deserunt dignissimos dolores fuga. Quidem, id, temporibus esse quos officiis ipsam explicabo sit tempora labore sapiente provident sint quis iste est. */}
+                            {/* </p> */}
+                        {/* </CardPanel> */}
+                    {/* </Col> */}
+                {/* </Row> */}
+            {/* </section> */}
         </div>
     );
 }
